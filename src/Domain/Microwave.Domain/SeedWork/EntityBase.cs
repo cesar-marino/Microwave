b4c191f58c88ab80855b3e3ac09720fb50284c3e
César <cesarmarino00@gmail.com>
2025-02-19 19:20:57 -0300

    add seed work

---
+++ src/Domain/Microwave.Domain/SeedWork/EntityBase.cs
@@ -0,0 +1,7 @@
+﻿namespace Microwave.Domain.SeedWork
+{
+    public abstract class EntityBase(Guid? id = null)
+    {
+        public Guid Id { get; } = id ?? Guid.NewGuid();
+    }
+}
---
+++ src/Domain/Microwave.Domain/SeedWork/IRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace Microwave.Domain.SeedWork
+{
+    public interface IRepository<TEntity> where TEntity : EntityBase
+    {
+        Task<TEntity> FindAsync(Guid id, CancellationToken cancellationToken = default);
+        Task InsertAsync(TEntity entity, CancellationToken cancellationToken = default);
+        Task RemoveAsync(TEntity entity, CancellationToken cancellationToken = default);
+        Task UpdateAsync(TEntity entity, CancellationToken cancellationToken = default);
+    }
+}
