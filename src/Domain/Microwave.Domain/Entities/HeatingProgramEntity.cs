ce13d4ab5c804b5a3b485edc8155a4ad7003177b
César <cesarmarino00@gmail.com>
2025-02-19 19:23:54 -0300

    add heating program entity

---
+++ src/Domain/Microwave.Domain/Entities/HeatingProgramEntity.cs
@@ -0,0 +1,15 @@
+﻿using Microwave.Domain.SeedWork;
+
+namespace Microwave.Domain.Entities
+{
+    public class HeatingProgramEntity : EntityBase
+    {
+        public bool Predefined { get; }
+        public int Seconds { get; }
+        public int Power { get; }
+        public string Name { get; }
+        public string Food { get; }
+        public char Character { get; }
+        public string? Instructions { get; }
+    }
+}
