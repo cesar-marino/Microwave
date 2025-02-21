46d516bccb48720003fd9714913fc389b0899a48
César <cesarmarino00@gmail.com>
2025-02-21 18:21:19 -0300

    add remove heating program uni tests

---
+++ test/Microwave.Test.UnitTest/Application/UseCases/HeatingProgram/RemoveHeatingProgram/RemoveHeatingProgramHandlerTest.cs
@@ -0,0 +1,6 @@
+﻿namespace Microwave.Test.UnitTest.Application.UseCases.HeatingProgram.RemoveHeatingProgram
+{
+    public class RemoveHeatingProgramHandlerTest : IClassFixture<RemoveHeatingProgramHandlerTestFixture>
+    {
+    }
+}
---
+++ test/Microwave.Test.UnitTest/Application/UseCases/HeatingProgram/RemoveHeatingProgram/RemoveHeatingProgramHandlerTestFixture.cs
@@ -0,0 +1,8 @@
+﻿using Microwave.Test.UnitTest.Commons;
+
+namespace Microwave.Test.UnitTest.Application.UseCases.HeatingProgram.RemoveHeatingProgram
+{
+    public class RemoveHeatingProgramHandlerTestFixture : FixtureBase
+    {
+    }
+}
