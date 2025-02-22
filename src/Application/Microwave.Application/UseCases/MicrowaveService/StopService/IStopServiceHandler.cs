0dab04c0fccf6dbe9b9ecda05887acb4d1239aaf
César <cesarmarino00@gmail.com>
2025-02-22 17:21:14 -0300

    add stop service use case

---
+++ src/Application/Microwave.Application/UseCases/MicrowaveService/StopService/IStopServiceHandler.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using Microwave.Application.UseCases.MicrowaveService.Commons;
+
+namespace Microwave.Application.UseCases.MicrowaveService.StopService
+{
+    public interface IStopServiceHandler : IRequestHandler<StopServiceRequest, MicrowaveServiceResponse>
+    {
+    }
+}
---
+++ src/Application/Microwave.Application/UseCases/MicrowaveService/StopService/StopServiceHandler.cs
@@ -0,0 +1,12 @@
+﻿using Microwave.Application.UseCases.MicrowaveService.Commons;
+
+namespace Microwave.Application.UseCases.MicrowaveService.StopService
+{
+    public class StopServiceHandler : IStopServiceHandler
+    {
+        public Task<MicrowaveServiceResponse> Handle(StopServiceRequest request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
