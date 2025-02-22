ac2019750ef4700a13c146c21e9f53e85f85d059
César <cesarmarino00@gmail.com>
2025-02-22 16:03:00 -0300

    ensure return the correctlyt processed heat string

---
+++ src/Domain/Microwave.Domain/Entities/MicrowaveServiceEntity.cs
@@ -0,0 +1,27 @@
+﻿using Microwave.Domain.Enums;
+using Microwave.Domain.SeedWork;
+
+namespace Microwave.Domain.Entities
+{
+    public class MicrowaveServiceEntity : EntityBase
+    {
+        public MicrowaveServiceStatus Status { get; }
+        public HeatingProgramEntity HeatingProgram { get; }
+
+        public MicrowaveServiceEntity(HeatingProgramEntity heatingProgram)
+        {
+            Status = MicrowaveServiceStatus.InProgress;
+            HeatingProgram = heatingProgram;
+        }
+
+        public string Process()
+        {
+            var processResult = string.Empty;
+
+            for (int i = 0; i < HeatingProgram.Power; i++)
+                processResult += HeatingProgram.Character;
+
+            return processResult;
+        }
+    }
+}
---
+++ test/Microwave.Test.UnitTest/Domain/Entities/MicrowaveService/MicrowaveServiceEntityTest.cs
@@ -0,0 +1,22 @@
+﻿using Microwave.Domain.Entities;
+
+namespace Microwave.Test.UnitTest.Domain.Entities.MicrowaveService
+{
+    public class MicrowaveServiceEntityTest
+    {
+        [Theory(DisplayName = nameof(ShouldReturnTheCorrectlyProcessedHeatString))]
+        [Trait("Unit/Entities", "MicrowaveProgram - Process")]
+        [InlineData(1, ".")]
+        [InlineData(5, ".....")]
+        [InlineData(10, "..........")]
+        public void ShouldReturnTheCorrectlyProcessedHeatString(int power, string result)
+        {
+            var heatingProgram = new HeatingProgramEntity(power: power);
+            var microwaveService = new MicrowaveServiceEntity(heatingProgram);
+
+            var response = microwaveService.Process();
+
+            Assert.Equal(result, response);
+        }
+    }
+}
