f3fb0ea283026bd8dcfd416ad615b211d72aac32
César <cesarmarino00@gmail.com>
2025-02-22 13:42:00 -0300

    add user entity

---
+++ src/Domain/Microwave.Domain/Entities/UserEntity.cs
@@ -0,0 +1,12 @@
+﻿using Microwave.Domain.SeedWork;
+
+namespace Microwave.Domain.Entities
+{
+    public class UserEntity(
+        string username,
+        string password) : EntityBase
+    {
+        public string Username { get; } = username;
+        public string Password { get; } = password;
+    }
+}
