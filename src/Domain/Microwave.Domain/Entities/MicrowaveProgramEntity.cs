955e9d669dbbd3f265ba85f5cbe8b35fdf3e43e4
César <cesarmarino00@gmail.com>
2025-02-22 15:34:33 -0300

    add microwave program entity

---
+++ src/Domain/Microwave.Domain/Entities/MicrowaveProgramEntity.cs
@@ -0,0 +1,17 @@
+﻿using Microwave.Domain.Enums;
+using Microwave.Domain.SeedWork;
+
+namespace Microwave.Domain.Entities
+{
+    public class MicrowaveProgramEntity : EntityBase
+    {
+        public MicrowaveServiceStatus Status { get; }
+        public HeatingProgramEntity HeatingProgram { get; }
+
+        public MicrowaveProgramEntity(HeatingProgramEntity heatingProgram)
+        {
+            Status = MicrowaveServiceStatus.InProgress;
+            HeatingProgram = heatingProgram;
+        }
+    }
+}
