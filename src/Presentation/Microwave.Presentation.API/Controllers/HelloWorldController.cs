967409b10e1c992ab0f9686e81e1bc6b19a94ce6
César <cesarmarino00@gmail.com>
2025-02-22 18:30:21 -0300

    add hello world controller

---
+++ src/Presentation/Microwave.Presentation.API/Controllers/HelloWorldController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Microwave.Presentation.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class HelloWorldController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult HelloWorld() => Ok("Hello World");
+    }
+}
--- src/Presentation/Microwave.Presentation.API/Program.cs
+++ src/Presentation/Microwave.Presentation.API/Program.cs
@@ -3,8 +3,13 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+//builder.Services.AddControllers(options => options.Filters.Add(typeof(ApiGlobalExceptionFilter))).AddJsonOptions();
+
+builder.Services.Configure<RouteOptions>(options => options.LowercaseUrls = true);
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -13,6 +18,9 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+
+app.UseSwagger();
+app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Microwave"));
 
 app.UseHttpsRedirection();
 
