f6300effafe02a6d2743900d73d9698d51c2c823
César <cesarmarino00@gmail.com>
2025-02-23 18:43:05 -0300

    add authentication integration test

---
+++ test/Microwave.Test.IntegrationTest/Application/UseCases/User/Auth/AuthenticationHandlerTestFixture.cs
@@ -0,0 +1,8 @@
+﻿using Microwave.Test.IntegrationTest.Commons;
+
+namespace Microwave.Test.IntegrationTest.Application.UseCases.User.Auth
+{
+    public class AuthenticationHandlerTestFixture : FixtureBase
+    {
+    }
+}
