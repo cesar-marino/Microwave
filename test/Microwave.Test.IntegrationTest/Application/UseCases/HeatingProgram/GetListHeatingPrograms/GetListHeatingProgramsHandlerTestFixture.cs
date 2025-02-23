0a94b147090cc5bbc3c0a7aeeb229404a0576aa8
César <cesarmarino00@gmail.com>
2025-02-23 16:05:42 -0300

    add get list heating programs integration tests

---
+++ test/Microwave.Test.IntegrationTest/Application/UseCases/HeatingProgram/GetListHeatingPrograms/GetListHeatingProgramsHandlerTestFixture.cs
@@ -0,0 +1,8 @@
+﻿using Microwave.Test.IntegrationTest.Commons;
+
+namespace Microwave.Test.IntegrationTest.Application.UseCases.HeatingProgram.GetListHeatingPrograms
+{
+    public class GetListHeatingProgramsHandlerTestFixture : FixtureBase
+    {
+    }
+}
