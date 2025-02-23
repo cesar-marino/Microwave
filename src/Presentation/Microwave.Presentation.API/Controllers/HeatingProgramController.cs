39629b37e97b32f38ca33c2dc73272a00181a9ce
César <cesarmarino00@gmail.com>
2025-02-23 15:02:06 -0300

    add heating program controller

---
+++ src/Presentation/Microwave.Presentation.API/Controllers/HeatingProgramController.cs
@@ -0,0 +1,50 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microwave.Application.UseCases.HeatingProgram.CreateHeatingProgram;
+using Microwave.Application.UseCases.HeatingProgram.GetListHeatingPrograms;
+using Microwave.Application.UseCases.HeatingProgram.RemoveHeatingProgram;
+using Microwave.Application.UseCases.HeatingProgram.UpdateHeatingProgram;
+
+namespace Microwave.Presentation.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HeatingProgramController(IMediator mediator) : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> Create(
+            [FromBody] CreateHeatingProgramRequest request,
+            CancellationToken cancellationToken = default)
+        {
+            var response = await mediator.Send(request, cancellationToken);
+            return Ok(response);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll(CancellationToken cancellationToken = default)
+        {
+            var request = new GetListHeatingProgramsRequest();
+            var response = await mediator.Send(request, cancellationToken);
+            return Ok(response);
+        }
+
+        [HttpDelete("id:Guid/remove")]
+        public async Task<IActionResult> Remove(
+            [FromRoute] Guid id,
+            CancellationToken cancellationToken = default)
+        {
+            var request = new RemoveHeatingProgramRequest(heatingProgramId: id);
+            await mediator.Send(request, cancellationToken);
+            return StatusCode(204);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(
+            [FromBody] UpdateHeatingProgramRequest request,
+            CancellationToken cancellationToken = default)
+        {
+            var response = await mediator.Send(request, cancellationToken);
+            return Ok(response);
+        }
+    }
+}
