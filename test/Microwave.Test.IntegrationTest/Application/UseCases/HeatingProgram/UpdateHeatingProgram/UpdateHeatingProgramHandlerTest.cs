a1c4b7c7b1c5a74ea2e57795c933a654ac26f049
César <cesarmarino00@gmail.com>
2025-02-23 18:35:35 -0300

    ensure throw ActionNotPermittedException

--- test/Microwave.Test.IntegrationTest/Application/UseCases/HeatingProgram/UpdateHeatingProgram/UpdateHeatingProgramHandlerTest.cs
+++ test/Microwave.Test.IntegrationTest/Application/UseCases/HeatingProgram/UpdateHeatingProgram/UpdateHeatingProgramHandlerTest.cs
@@ -1,6 +1,59 @@
-﻿namespace Microwave.Test.IntegrationTest.Application.UseCases.HeatingProgram.UpdateHeatingProgram
+﻿using Microwave.Application.UseCases.HeatingProgram.UpdateHeatingProgram;
+using Microwave.Domain.Exceptions;
+using Microwave.Infrastructure.Data.Contexts;
+using Microwave.Infrastructure.Data.Repositories;
+
+namespace Microwave.Test.IntegrationTest.Application.UseCases.HeatingProgram.UpdateHeatingProgram
 {
-    public class UpdateHeatingProgramHandlerTest : IClassFixture<UpdateHeatingProgramHandlerTestFixture>
+    public class UpdateHeatingProgramHandlerTest(UpdateHeatingProgramHandlerTestFixture fixture) : IClassFixture<UpdateHeatingProgramHandlerTestFixture>
     {
+        private readonly UpdateHeatingProgramHandlerTestFixture _fixture = fixture;
+
+        [Fact(DisplayName = nameof(ShouldThroNotFoundException))]
+        [Trait("Integration/UseCase", "HeatingProgram - UpdateHeatingProgram")]
+        public async Task ShouldThroNotFoundException()
+        {
+            var context = _fixture.MakeMicrowaveContext();
+            var unitOfWork = new UnitOfWork(context);
+            var repository = new HeatingProgramRepository(context);
+
+            var sut = new UpdateHeatingProgramHandler(
+                heatingProgramRepository: repository,
+                unitOfWork: unitOfWork);
+
+            var request = _fixture.MakeUpdateHeatingProgramRequest();
+            var act = () => sut.Handle(request, _fixture.CancellationToken);
+
+            var exception = await Assert.ThrowsAsync<NotFoundException>(act);
+            Assert.Equal("not-found", exception.Code);
+            Assert.Equal("Programa não encontrado", exception.Message);
+        }
+
+        [Fact(DisplayName = nameof(ShouldThrowActionNotPermittedException))]
+        [Trait("Integration/UseCase", "HeatingProgram - UpdateHeatingProgram")]
+        public async Task ShouldThrowActionNotPermittedException()
+        {
+            var context = _fixture.MakeMicrowaveContext();
+            var heatingProgram = _fixture.MakeHeatingProgramEntity();
+            await context.HeatingPrograms.AddAsync(heatingProgram);
+            await context.SaveChangesAsync();
+
+            var unitOfWork = new UnitOfWork(context);
+            var repository = new HeatingProgramRepository(context);
+
+            var sut = new UpdateHeatingProgramHandler(
+                heatingProgramRepository: repository,
+                unitOfWork: unitOfWork);
+
+            var request = _fixture.MakeUpdateHeatingProgramRequest(
+                heatingProgramId: heatingProgram.Id,
+                character: heatingProgram.Character);
+
+            var act = () => sut.Handle(request, _fixture.CancellationToken);
+
+            var exception = await Assert.ThrowsAsync<ActionNotPermittedException>(act);
+            Assert.Equal("action-not-permitted", exception.Code);
+            Assert.Equal("Caractere de aquecimento em uso", exception.Message);
+        }
     }
 }
--- test/Microwave.Test.IntegrationTest/Application/UseCases/HeatingProgram/UpdateHeatingProgram/UpdateHeatingProgramHandlerTestFixture.cs
+++ test/Microwave.Test.IntegrationTest/Application/UseCases/HeatingProgram/UpdateHeatingProgram/UpdateHeatingProgramHandlerTestFixture.cs
@@ -1,8 +1,19 @@
-﻿using Microwave.Test.IntegrationTest.Commons;
+﻿using Microwave.Application.UseCases.HeatingProgram.UpdateHeatingProgram;
+using Microwave.Test.IntegrationTest.Commons;
 
 namespace Microwave.Test.IntegrationTest.Application.UseCases.HeatingProgram.UpdateHeatingProgram
 {
     public class UpdateHeatingProgramHandlerTestFixture : FixtureBase
     {
+        public UpdateHeatingProgramRequest MakeUpdateHeatingProgramRequest(
+            Guid? heatingProgramId = null,
+            char? character = null) => new(
+                heatingProgramId: heatingProgramId ?? Faker.Random.Guid(),
+                seconds: Faker.Random.Int(1, 120),
+                power: Faker.Random.Int(1, 10),
+                character: character ?? Faker.Random.Char(),
+                name: Faker.Random.String(),
+                food: Faker.Random.String(),
+                instructions: Faker.Random.String());
     }
 }
