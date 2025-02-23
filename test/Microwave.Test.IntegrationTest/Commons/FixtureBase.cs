4ea99be5c24f3e8e968c865975ceb496a803f4a6
César <cesarmarino00@gmail.com>
2025-02-23 15:50:44 -0300

    add initial configuration

---
+++ test/Microwave.Test.IntegrationTest/Commons/FixtureBase.cs
@@ -0,0 +1,10 @@
+﻿using Bogus;
+
+namespace Microwave.Test.IntegrationTest.Commons
+{
+    public abstract class FixtureBase
+    {
+        public CancellationToken CancellationToken { get; } = default;
+        public Faker Faker { get; } = new();
+    }
+}
