84cc3013a7dab13b135295d3e83b063ce773b886
César <cesarmarino00@gmail.com>
2025-02-23 19:02:19 -0300

    add create user integration tests

---
+++ test/Microwave.Test.IntegrationTest/Application/UseCases/User/CreateUser/CreateUserHandlerTest.cs
@@ -0,0 +1,6 @@
+﻿namespace Microwave.Test.IntegrationTest.Application.UseCases.User.CreateUser
+{
+    public class CreateUserHandlerTest : IClassFixture<CreateUserHandlerTestFixture>
+    {
+    }
+}
---
+++ test/Microwave.Test.IntegrationTest/Application/UseCases/User/CreateUser/CreateUserHandlerTestFixture.cs
@@ -0,0 +1,8 @@
+﻿using Microwave.Test.IntegrationTest.Commons;
+
+namespace Microwave.Test.IntegrationTest.Application.UseCases.User.CreateUser
+{
+    public class CreateUserHandlerTestFixture : FixtureBase
+    {
+    }
+}
