4a223e9d5adf88c06f9b2d5cae87cc2a5e8a45f8
César <cesarmarino00@gmail.com>
2025-02-23 19:08:37 -0300

    ensure throw UsernameInUseException

--- test/Microwave.Test.IntegrationTest/Application/UseCases/User/CreateUser/CreateUserHandlerTest.cs
+++ test/Microwave.Test.IntegrationTest/Application/UseCases/User/CreateUser/CreateUserHandlerTest.cs
@@ -1,6 +1,44 @@
-﻿namespace Microwave.Test.IntegrationTest.Application.UseCases.User.CreateUser
+﻿using Microwave.Application.UseCases.User.CreateUser;
+using Microwave.Domain.Exceptions;
+using Microwave.Infrastructure.Data.Contexts;
+using Microwave.Infrastructure.Data.Repositories;
+using Microwave.Infrastructure.Services.Encryption;
+using Microwave.Infrastructure.Services.Token;
+
+namespace Microwave.Test.IntegrationTest.Application.UseCases.User.CreateUser
 {
-    public class CreateUserHandlerTest : IClassFixture<CreateUserHandlerTestFixture>
+    public class CreateUserHandlerTest(CreateUserHandlerTestFixture fixture) : IClassFixture<CreateUserHandlerTestFixture>
     {
+        private readonly CreateUserHandlerTestFixture _fixture = fixture;
+
+        [Fact(DisplayName = nameof(ShouldThrowUsernameInUseException))]
+        [Trait("Integration/UseCase", "User - CreateUser")]
+        public async Task ShouldThrowUsernameInUseException()
+        {
+            var user = _fixture.MakeUserEntity();
+            var context = _fixture.MakeMicrowaveContext();
+            await context.Users.AddAsync(user);
+            await context.SaveChangesAsync();
+
+            var encryptionService = new EncryptionService();
+            var tokenService = new TokenService(_fixture.MakeConfiguration());
+
+            var unitOfWork = new UnitOfWork(context);
+            var repository = new UserRepository(context);
+
+            var sut = new CreateUserHandler(
+                userRepository: repository,
+                encryptionService: encryptionService,
+                tokenService: tokenService,
+                unitOfWork: unitOfWork);
+
+            var request = _fixture.MakeCreateUserRequest(username: user.Username);
+            var act = () => sut.Handle(request, _fixture.CancellationToken);
+
+            var exception = await Assert.ThrowsAsync<UsernameInUseException>(act);
+            Assert.Equal("username-in-use", exception.Code);
+            Assert.Equal("Username já cadastrado para outro usuário", exception.Message);
+
+        }
     }
 }
--- test/Microwave.Test.IntegrationTest/Application/UseCases/User/CreateUser/CreateUserHandlerTestFixture.cs
+++ test/Microwave.Test.IntegrationTest/Application/UseCases/User/CreateUser/CreateUserHandlerTestFixture.cs
@@ -1,8 +1,14 @@
-﻿using Microwave.Test.IntegrationTest.Commons;
+﻿using Microwave.Application.UseCases.User.CreateUser;
+using Microwave.Test.IntegrationTest.Commons;
 
 namespace Microwave.Test.IntegrationTest.Application.UseCases.User.CreateUser
 {
     public class CreateUserHandlerTestFixture : FixtureBase
     {
+        public CreateUserRequest MakeCreateUserRequest(
+            string? username = null,
+            string? password = null) => new(
+                username: username ?? Faker.Internet.UserName(),
+                password: password ?? Faker.Internet.Password());
     }
 }
