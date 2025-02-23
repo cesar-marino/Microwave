14c2b7bd5498d9bd9d497364a1e701cecf7bdce2
César <cesarmarino00@gmail.com>
2025-02-23 18:13:37 -0300

    ensure return empty list

--- test/Microwave.Test.IntegrationTest/Application/UseCases/HeatingProgram/GetListHeatingPrograms/GetListHeatingProgramsHandlerTest.cs
+++ test/Microwave.Test.IntegrationTest/Application/UseCases/HeatingProgram/GetListHeatingPrograms/GetListHeatingProgramsHandlerTest.cs
@@ -1,12 +1,24 @@
-﻿namespace Microwave.Test.IntegrationTest.Application.UseCases.HeatingProgram.GetListHeatingPrograms
-{
-    public class GetListHeatingProgramsHandlerTest : IClassFixture<GetListHeatingProgramsHandlerTestFixture>
-    {
-        [Fact(DisplayName = nameof(ShouldReturnSameResultThatGetAllAsync))]
-        [Trait("Integration/UseCase", "HeatingProgram - GetListHeatingPrograms")]
-        public async Task ShouldReturnSameResultThatGetAllAsync()
-        {
+﻿using Microwave.Application.UseCases.HeatingProgram.GetListHeatingPrograms;
+using Microwave.Infrastructure.Data.Repositories;
 
+namespace Microwave.Test.IntegrationTest.Application.UseCases.HeatingProgram.GetListHeatingPrograms
+{
+    public class GetListHeatingProgramsHandlerTest(GetListHeatingProgramsHandlerTestFixture fixture) : IClassFixture<GetListHeatingProgramsHandlerTestFixture>
+    {
+        private readonly GetListHeatingProgramsHandlerTestFixture _fixture = fixture;
+
+        [Fact(DisplayName = nameof(ShouldReturnEmptyList))]
+        [Trait("Integration/UseCase", "HeatingProgram - GetListHeatingPrograms")]
+        public async Task ShouldReturnEmptyList()
+        {
+            var context = _fixture.MakeMicrowaveContext();
+            var repository = new HeatingProgramRepository(context);
+            var sut = new GetListHeatingProgramsHandler(heatingProgramRepository: repository);
+
+            var request = _fixture.MakeGetListHeatingPrograms();
+            var response = await sut.Handle(request, _fixture.CancellationToken);
+
+            Assert.Empty(response);
         }
     }
 }
--- test/Microwave.Test.IntegrationTest/Application/UseCases/HeatingProgram/GetListHeatingPrograms/GetListHeatingProgramsHandlerTestFixture.cs
+++ test/Microwave.Test.IntegrationTest/Application/UseCases/HeatingProgram/GetListHeatingPrograms/GetListHeatingProgramsHandlerTestFixture.cs
@@ -1,8 +1,10 @@
-﻿using Microwave.Test.IntegrationTest.Commons;
+﻿using Microwave.Application.UseCases.HeatingProgram.GetListHeatingPrograms;
+using Microwave.Test.IntegrationTest.Commons;
 
 namespace Microwave.Test.IntegrationTest.Application.UseCases.HeatingProgram.GetListHeatingPrograms
 {
     public class GetListHeatingProgramsHandlerTestFixture : FixtureBase
     {
+        public GetListHeatingProgramsRequest MakeGetListHeatingPrograms() => new();
     }
 }
