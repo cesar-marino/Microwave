ada460fc9a661e21a37adf0b01189d8a93bbf7a9
César <cesarmarino00@gmail.com>
2025-02-23 12:15:07 -0300

    add desktop client project

---
+++ src/Presentation/Microwave.Presentation.DesktopClient/Form1.cs
@@ -0,0 +1,10 @@
+namespace Microwave.Presentation.DesktopClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
