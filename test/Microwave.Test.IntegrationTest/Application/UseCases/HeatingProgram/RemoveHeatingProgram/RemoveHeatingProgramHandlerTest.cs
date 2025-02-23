24fe433749c249d81dff2871c20bcbc1f1a9bdd2
César <cesarmarino00@gmail.com>
2025-02-23 18:23:39 -0300

    ensure throw NotFoundException

--- test/Microwave.Test.IntegrationTest/Application/UseCases/HeatingProgram/RemoveHeatingProgram/RemoveHeatingProgramHandlerTest.cs
+++ test/Microwave.Test.IntegrationTest/Application/UseCases/HeatingProgram/RemoveHeatingProgram/RemoveHeatingProgramHandlerTest.cs
@@ -1,6 +1,32 @@
-﻿namespace Microwave.Test.IntegrationTest.Application.UseCases.HeatingProgram.RemoveHeatingProgram
+﻿using Microwave.Application.UseCases.HeatingProgram.RemoveHeatingProgram;
+using Microwave.Domain.Exceptions;
+using Microwave.Infrastructure.Data.Contexts;
+using Microwave.Infrastructure.Data.Repositories;
+
+namespace Microwave.Test.IntegrationTest.Application.UseCases.HeatingProgram.RemoveHeatingProgram
 {
-    public class RemoveHeatingProgramHandlerTest : IClassFixture<RemoveHeatingProgramHandlerTestFixture>
+    public class RemoveHeatingProgramHandlerTest(RemoveHeatingProgramHandlerTestFixture fixture) : IClassFixture<RemoveHeatingProgramHandlerTestFixture>
     {
+        private readonly RemoveHeatingProgramHandlerTestFixture _fixture = fixture;
+
+        [Fact(DisplayName = nameof(ShouldThrowNotFoundException))]
+        [Trait("Integration/UseCase", "HeatingProgram - RemoveHeatingProgram")]
+        public async Task ShouldThrowNotFoundException()
+        {
+            var context = _fixture.MakeMicrowaveContext();
+            var unitOfWork = new UnitOfWork(context);
+            var repository = new HeatingProgramRepository(context);
+
+            var sut = new RemoveHeatingProgramHandler(
+                heatingProgramRepository: repository,
+                unitOfWork: unitOfWork);
+
+            var request = _fixture.MakeRemoveHeatingPRogramRequest();
+            var act = () => sut.Handle(request, _fixture.CancellationToken);
+
+            var exception = await Assert.ThrowsAsync<NotFoundException>(act);
+            Assert.Equal("not-found", exception.Code);
+            Assert.Equal("Programa não encontrado", exception.Message);
+        }
     }
 }
--- test/Microwave.Test.IntegrationTest/Application/UseCases/HeatingProgram/RemoveHeatingProgram/RemoveHeatingProgramHandlerTestFixture.cs
+++ test/Microwave.Test.IntegrationTest/Application/UseCases/HeatingProgram/RemoveHeatingProgram/RemoveHeatingProgramHandlerTestFixture.cs
@@ -1,8 +1,11 @@
-﻿using Microwave.Test.IntegrationTest.Commons;
+﻿using Microwave.Application.UseCases.HeatingProgram.RemoveHeatingProgram;
+using Microwave.Test.IntegrationTest.Commons;
 
 namespace Microwave.Test.IntegrationTest.Application.UseCases.HeatingProgram.RemoveHeatingProgram
 {
     public class RemoveHeatingProgramHandlerTestFixture : FixtureBase
     {
+        public RemoveHeatingProgramRequest MakeRemoveHeatingPRogramRequest(Guid? heatingProgramId = null) =>
+            new(heatingProgramId: heatingProgramId ?? Faker.Random.Guid());
     }
 }
