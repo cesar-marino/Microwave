93a81021927adaabe42f7dc4c1e3fdd0ae2da3d4
César <cesarmarino00@gmail.com>
2025-02-23 18:19:32 -0300

    add remove heating program integration test

---
+++ test/Microwave.Test.IntegrationTest/Application/UseCases/HeatingProgram/RemoveHeatingProgram/RemoveHeatingProgramHandlerTest.cs
@@ -0,0 +1,6 @@
+﻿namespace Microwave.Test.IntegrationTest.Application.UseCases.HeatingProgram.RemoveHeatingProgram
+{
+    public class RemoveHeatingProgramHandlerTest : IClassFixture<RemoveHeatingProgramHandlerTestFixture>
+    {
+    }
+}
---
+++ test/Microwave.Test.IntegrationTest/Application/UseCases/HeatingProgram/RemoveHeatingProgram/RemoveHeatingProgramHandlerTestFixture.cs
@@ -0,0 +1,8 @@
+﻿using Microwave.Test.IntegrationTest.Commons;
+
+namespace Microwave.Test.IntegrationTest.Application.UseCases.HeatingProgram.RemoveHeatingProgram
+{
+    public class RemoveHeatingProgramHandlerTestFixture : FixtureBase
+    {
+    }
+}
