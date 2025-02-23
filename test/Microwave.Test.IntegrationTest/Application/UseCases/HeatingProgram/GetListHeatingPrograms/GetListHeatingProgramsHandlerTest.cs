4d2eb03223a7ac58ed2b40783cf84deb1fbfd4e7
César <cesarmarino00@gmail.com>
2025-02-23 16:06:52 -0300

    .

--- test/Microwave.Test.IntegrationTest/Application/UseCases/HeatingProgram/GetListHeatingPrograms/GetListHeatingProgramsHandlerTest.cs
+++ test/Microwave.Test.IntegrationTest/Application/UseCases/HeatingProgram/GetListHeatingPrograms/GetListHeatingProgramsHandlerTest.cs
@@ -2,5 +2,11 @@
 {
     public class GetListHeatingProgramsHandlerTest : IClassFixture<GetListHeatingProgramsHandlerTestFixture>
     {
+        [Fact(DisplayName = nameof(ShouldReturnSameResultThatGetAllAsync))]
+        [Trait("Integration/UseCase", "HeatingProgram - GetListHeatingPrograms")]
+        public async Task ShouldReturnSameResultThatGetAllAsync()
+        {
+
+        }
     }
 }
